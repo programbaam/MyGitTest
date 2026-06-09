4bd67ea3bb08869c5ecf61437625837552a82628
이범호 <96612703+programbaam@users.noreply.github.com>
2026-06-09 21:28:53 +0900

    두 번째 커밋
    두 번째 커밋

---
+++ Source/MyGitTest/OneActor.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "OneActor.h"
+
+// Sets default values
+AOneActor::AOneActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AOneActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AOneActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
